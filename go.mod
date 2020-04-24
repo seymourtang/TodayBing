af0d0b34ca2454f7a13d1978cf1751e0f6edaf87
Tang Xianmeng <tangxianmeng@live.com>
2020-04-24 14:32:53 +0800

    Replace standard json parser with json-iterator lib.

--- go.mod
+++ go.mod
@@ -4,4 +4,5 @@ go 1.14
 
 require (
 	github.com/gin-gonic/gin v1.6.2
+	github.com/json-iterator/go v1.1.9
 )
--- main.go
+++ main.go
@@ -1,12 +1,10 @@
 package main
 
 import (
-	"encoding/json"
 	"github.com/gin-gonic/gin"
-	"io/ioutil"
+	"github.com/json-iterator/go"
 	"log"
 	"net/http"
-	"sort"
 	"strconv"
 )
 
@@ -20,18 +18,6 @@ type Image struct {
 type Response struct {
 	Image []*Image `json:"images"`
 }
-type ImageList []*Image
-
-func (i ImageList) Len() int {
-	return len(i)
-}
-
-func (i ImageList) Swap(m, n int) {
-	i[m], i[n] = i[n], i[m]
-}
-func (i ImageList) Less(m, n int) bool {
-	return i[m].Index < i[n].Index
-}
 
 const (
 	urlBase = "https://cn.bing.com/"
@@ -41,24 +27,23 @@ func main() {
 	gin.SetMode(gin.ReleaseMode)
 	r := gin.Default()
 	r.GET("/v1/todaybing", GetLatest7Days)
-	if err := http.ListenAndServe(":8088", r); err != nil {
+	if err := http.ListenAndServe(":5033", r); err != nil {
 		log.Println(err)
 	}
 }
 
 func GetLatest7Days(ctx *gin.Context) {
 	ch := make(chan Response, 7)
-	urls := make([]*Image, 0, 7)
+	urls := make([]*Image, 7)
 	for i := 0; i < 7; i++ {
 		url := "https://cn.bing.com/HPImageArchive.aspx?format=js&idx=" + strconv.Itoa(i) + "&n=1&mkt=zh-CN"
 		go GetLatestDay(url, i, ch)
 	}
 	for i := 0; i < 7; i++ {
 		data := <-ch
-		urls = append(urls, data.Image[0])
+		urls[data.Image[0].Index] = data.Image[0]
 	}
 	close(ch)
-	sort.Sort(ImageList(urls))
 	ctx.JSON(200, urls)
 }
 
@@ -69,9 +54,9 @@ func GetLatestDay(url string, index int, ch chan Response) {
 		ch <- Response{}
 	}
 	defer resp.Body.Close()
-	body, _ := ioutil.ReadAll(resp.Body)
 	var response Response
-	if err := json.Unmarshal(body, &response); err != nil {
+	var json = jsoniter.ConfigCompatibleWithStandardLibrary
+	if err := json.NewDecoder(resp.Body).Decode(&response); err != nil {
 		log.Println(err)
 		ch <- Response{}
 	}
